861056a84efe6a47c59cc67e3867ef903ccf3449
GiangHoGoVap <58210406+GiangHoGoVap@users.noreply.github.com>
2020-01-06 21:25:56 +0700

    Update PowerOfNumber.py

--- PowerOfNumber/PowerOfNumber.py
+++ PowerOfNumber/PowerOfNumber.py
@@ -1,4 +1,5 @@
-def isPowerOfThree(n1,n2):
+#Python program to check if the number x is power of y 
+def isPower(n1,n2):
     if (n1==0):
         return False
     while (n1!=1):
@@ -9,7 +10,7 @@ def isPowerOfThree(n1,n2):
 
 x=int(input("Enter a number: "))
 y=int(input("Enter the number of power: "))
-if(isPowerOfThree(x,y)):
+if(isPower(x,y)):
     print("This number is power of ",y)
 else:
     print("This number is not")
